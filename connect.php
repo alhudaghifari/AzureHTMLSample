1604f3fb9835be1d8140276b5237d0da396d7f1d
alhudaghifari <alhudaghifari@gmail.com>
2019-07-03 11:08:55 +0700

    servername added

--- connect.php
+++ connect.php
@@ -5,9 +5,10 @@
     $db_name = "dicodingdb";
 
     $serverName = "latiandicoding.database.windows.net";  
+    $serverName2 = "latiandicodingwndws.database.windows.net";
     $connectionOptions = array("Database"=>$db_name,  
         "Uid"=>$username, "PWD"=>$password);  
-    $conn = sqlsrv_connect($serverName, $connectionOptions);  
+    $conn = sqlsrv_connect($serverName2, $connectionOptions);  
     if($conn == false)  {
         echo "Error bos!";  
 	    die("Connection failed: " . $conn->connect_error . " </br>" . FormatErrors(sqlsrv_errors()));
