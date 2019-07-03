4fe198e8cc7753cdb955c3f21a37f4b3e343af86
alhudaghifari <alhudaghifari@gmail.com>
2019-07-03 08:09:47 +0700

    data has been read

--- connect.php
+++ connect.php
@@ -1,71 +1,15 @@
 <?php
-	// $servername = "latiandicoding.database.windows.net";
-	// $db_username = "dicoding";
-    // $db_password = "KambingJantan123";
-    // $db_name = "[dbo].[user]";
 
-	// $conn = new mysqli($servername, $db_username, $db_password, $db_name);
-	// if ($conn->connect_error) {
-	//     die("Connection failed: " . $conn->connect_error);
-	// } 
-
-    $host = "latiandicoding.database.windows.net";
     $username = "dicoding";
     $password = "KambingJantan123";
     $db_name = "dicodingdb";
-    
-    //Establishes the connection
-    // $conn = mysqli_init();
-    // mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
-    // if (mysqli_connect_errno($conn)) {
-    // die('Failed to connect to MySQL: '.mysqli_connect_error());
-    // }
 
-    // PHP Data Objects(PDO) Sample Code:
-    // try {
-    //     $conn = new PDO("sqlsrv:server = tcp:latiandicoding.database.windows.net,1433; Database = dicodingdb", "dicoding", $password);
-    //     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // }
-    // catch (PDOException $e) {
-    //     print("Error connecting to SQL Server. cuy : ");
-    //     die(print_r($e));
-    // }
-
-    // // SQL Server Extension Sample Code:
-    // $connectionInfo = array("UID" => "dicoding@latiandicoding", "pwd" => $password, "Database" => "dicodingdb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-    // $serverName = "tcp:latiandicoding.database.windows.net,1433";
-    // $conn = sqlsrv_connect($serverName, $connectionInfo);
-
-    echo "nyobain dulu";
-    try  
-    {  
-        $serverName = "tcp:latiandicoding.database.windows.net,1433";  
-        $connectionOptions = array("Database"=>"dicodingdb",  
-            "Uid"=>"dicoding", "PWD"=>"KambingJantan123");  
-        $conn = sqlsrv_connect($serverName, $connectionOptions);  
-        if($conn == false)  
-            die(FormatErrors(sqlsrv_errors()));  
-    }  
-    catch(Exception $e)  
-    {  
-        echo("Error!");  
-    }  
-
-    // PHP Data Objects(PDO) Sample Code:
-    // try {
-    //     $conn = new PDO("sqlsrv:server = tcp:latiandicoding.database.windows.net,1433; Database = dicodingdb", "dicoding", $password);
-    //     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // }
-    // catch (PDOException $e) {
-    //     print("Error connecting to SQL Server.");
-    //     die(print_r($e));
-    // }
-
-    // // SQL Server Extension Sample Code:
-    // $connectionInfo = array("UID" => "dicoding@latiandicoding", "pwd" => $password, "Database" => "dicodingdb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-    // $serverName = "tcp:latiandicoding.database.windows.net,1433";
-    // $conn = sqlsrv_connect($serverName, $connectionInfo);
-
-
-    echo "beres nyobain dulu";
+    $serverName = "tcp:latiandicoding.database.windows.net,1433";  
+    $connectionOptions = array("Database"=>$db_name,  
+        "Uid"=>$username, "PWD"=>$password);  
+    $conn = sqlsrv_connect($serverName, $connectionOptions);  
+    if($conn == false)  {
+        die(FormatErrors(sqlsrv_errors()));  
+        echo("Error bos!");  
+    }
 ?>
--- readuser.php
+++ readuser.php
@@ -2,42 +2,48 @@
 
 include("connect.php");
 
-echo "cek satu";
-$sql = "SELECT * FROM User";
-$result = $conn->query($sql);
+ //Establishes the connection
+ $tsql= "SELECT * FROM [dbo].[user]";
+ $getResults= sqlsrv_query($conn, $tsql);
+ if ($getResults == FALSE)
+     echo (sqlsrv_errors());
 
+     echo "
+     
+        <div class=\"container\">
+     <table class=\"table\">
+     <thead>
+         <tr>
+           <th>id</th>
+           <th>Name</th>
+           <th>Email</th>
+           <th>Job</th>
+           <th>Date</th>
+         </tr>
+       </thead>
+       <tbody>";
 
-echo "cek dua";
+ while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+    $stringdatetime=$row["Date"]->format('Y-m-d H:i:s');
 
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " - Name: " . $row["Name"]. "  - Email : " . $row["Email"] . " - Job : " . "<br>";
-    }
-} else {
-    echo "0 results";
-}
-echo "hasilnya mana bos";
-$conn->close();
-mysqli_close($conn);
+    echo "
+    <tr>
+      <td>" . $row["id"] . "</td>
+      <td>" . $row["Name"] . "</td>
+      <td>" . $row["Email"] . "</td>
+      <td>" . $row["Job"] . "</td>
+      <td>" . $stringdatetime . "</td>
+    </tr>";
+    
+//   echo "id: " . $row["id"]. " - Name: " . $row["Name"] . 
+//         " - Email : " . $row["Email"] . " - Job : " . $row["Job"] . 
+//         " - Date : " . $stringdatetime . "<br>";  
 
-// $serverName = "latiandicoding.database.windows.net"; // update me
-// $connectionOptions = array(
-//     "Database" => "user", // update me
-//     "Uid" => "Dicoding", // update me
-//     "PWD" => "KambingJantan123" // update me
-// );
-// //Establishes the connection
-// $conn = sqlsrv_connect($serverName, $connectionOptions);
-// $tsql= "SELECT * from User";
-// $getResults= sqlsrv_query($conn, $tsql);
-// echo ("Reading data from table" . PHP_EOL);
-// if ($getResults == FALSE)
-//     echo (sqlsrv_errors());
-// while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
-// //  echo ($row['CategoryName'] . " " . $row['ProductName'] . PHP_EOL);
-//     echo "id: " . $row["id"]. " - Name: " . $row["Name"]. "  - Email : " . $row["Email"] . " - Job : " . "<br>";
-// }
-// sqlsrv_free_stmt($getResults);
+ }
+echo "</tbody
+</table>
+</div>";
+
+ sqlsrv_free_stmt($getResults);
 
 ?>
