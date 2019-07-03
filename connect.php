359762188db524d3f95e36367a74af4144dda430
alhudaghifari <alhudaghifari@gmail.com>
2019-07-03 08:44:21 +0700

    debug

--- connect.php
+++ connect.php
@@ -4,12 +4,12 @@
     $password = "KambingJantan123";
     $db_name = "dicodingdb";
 
-    $serverName = "latiandicoding.database.windows.net,1433";  
+    $serverName = "tcp:latiandicoding.database.windows.net,1433";  
     $connectionOptions = array("Database"=>$db_name,  
         "Uid"=>$username, "PWD"=>$password);  
     $conn = sqlsrv_connect($serverName, $connectionOptions);  
     if($conn == false)  {
+        echo "Error bos!";  
         die(FormatErrors(sqlsrv_errors()));  
-        echo("Error bos!");  
     }
 ?>
