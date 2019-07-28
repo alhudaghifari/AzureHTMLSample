632c4ecc5e495b89e884fe1defb55b020da9226d
alhudaghifari <alhudaghifari@gmail.com>
2019-07-29 01:37:25 +0700

    upload image to blob added

---
+++ uploadimage.php
@@ -0,0 +1,67 @@
+
+<?php
+require_once "./random_string.php";
+
+$target_dir = "uploads/";
+$target_file = "img" . generateRandomString() . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+
+// Check if image file is a actual image or fake image
+if (isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if ($check !== false) {
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 2500000) {
+    echo "Sorry, your file is too large. Max 2.5 Mb";
+    $uploadOk = 0;
+}
+
+// Allow certain file formats
+if (
+    $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+    && $imageFileType != "gif"
+) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+}
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+    // if everything is ok, try to upload file
+} else {
+    include("connecttoblob.php");
+    try {
+        $fileToUpload = $_FILES["fileToUpload"]["tmp_name"];
+        $myfile = fopen($fileToUpload, "r") or die("Unable to open file!");
+        fclose($myfile);
+        $content = fopen($fileToUpload, "r");
+        //Mengunggah blob
+        $blobClient->createBlockBlob($containerName, $target_file, $content);
+        header("Location: http://localhost/AzureHTMLSample/imagepage.php");
+        die();
+    } catch (ServiceException $e) {
+        // Handle exception based on error codes and messages.
+        // Error codes and messages are here:
+        // http://msdn.microsoft.com/library/azure/dd179439.aspx
+        $code = $e->getCode();
+        $error_message = $e->getMessage();
+        echo $code . ": " . $error_message . "<br />";
+    } catch (InvalidArgumentTypeException $e) {
+        // Handle exception based on error codes and messages.
+        // Error codes and messages are here:
+        // http://msdn.microsoft.com/library/azure/dd179439.aspx
+        $code = $e->getCode();
+        $error_message = $e->getMessage();
+        echo $code . ": " . $error_message . "<br />";
+    }
+}
+?>
