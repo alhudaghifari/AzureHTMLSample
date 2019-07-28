0375484fc1e6dd63b8e563ef469cc5252f340925
alhudaghifari <alhudaghifari@gmail.com>
2019-07-29 01:33:56 +0700

    composer and js added

--- js/logic.js
+++ js/logic.js
@@ -1,4 +1,4 @@
 var btn = document.getElementById("btnToDetectImage");
 btn.addEventListener("click", function() {
-  document.location.href = "detectimage.php";
+  document.location.href = "imagepage.php";
 });
